d3c3a55e6fb836399dc18876b2fe16e0f21182d1
Youssefsraidi <youssef.sraidi21@gmail.com>
2024-11-11 02:56:40 +0100

    data transformation commit

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from mlProject import logger
-from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from mlProject.pipeline.stage_01_data_ingestion import (DataIngestionTrainingPipeline)
+from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+
 
 
 logger.info("welcome to our custom logging")
@@ -13,3 +16,27 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+
+STAGE_NAME = "Data Validation stage"
+try:
+    logger.info(f">>>>>>>> stage {STAGE_NAME} started  <<<<<<")
+    obj = DataValidationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==============x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
+STAGE_NAME = "Data transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataTransformationTrainingPipeline()
+    obj.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+    
